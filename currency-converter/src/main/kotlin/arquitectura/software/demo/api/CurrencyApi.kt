f311749068e1fc8fbc7b0ef9c325e769314c4ed5
CarlosZ360 <carlos.zarate@ucb.edu.bo>
2023-03-21 01:30:01 -0400

    spring-security

--- currency-converter/src/main/kotlin/arquitectura/software/demo/api/CurrencyApi.kt
+++ currency-converter/src/main/kotlin/arquitectura/software/demo/api/CurrencyApi.kt
@@ -5,12 +5,14 @@ import arquitectura.software.demo.dto.ResponseServiceDto
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import java.math.BigDecimal
 import java.math.BigInteger
+import java.security.Principal
 
 @RequestMapping("/api/currency")
 @RestController
@@ -34,5 +36,23 @@ class CurrencyApi @Autowired constructor(private val currencyBl: CurrencyBl) {
         val result = currencyBl.history(page, size)
         return result
     }
+    @GetMapping("/user")
+    @PreAuthorize("hasAuthority('ROLE_USER')")
+    fun user(): String {
+        LOGGER.info("Iniciando servicio user")
+        return "ROLE_USER"
+    }
+
+    @GetMapping("/admin")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+    fun admin(): String {
+        LOGGER.info("Iniciando servicio admin")
+        return "ROLE_ADMIN"
+    }
+    @GetMapping("/principal")
+    fun info(principal: Principal): String {
+        return principal.toString()
+    }
+
 
 }
