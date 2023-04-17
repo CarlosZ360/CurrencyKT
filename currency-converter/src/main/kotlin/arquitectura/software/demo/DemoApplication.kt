e262f6ac4626c8a0cb79c2b46ef1b0ea4e1d8a2f
CarlosZ360 <carlos.zarate@ucb.edu.bo>
2023-04-17 16:44:00 -0400

    Agregado de zipkin y rabbitMq

--- currency-converter/src/main/kotlin/arquitectura/software/demo/DemoApplication.kt
+++ currency-converter/src/main/kotlin/arquitectura/software/demo/DemoApplication.kt
@@ -3,12 +3,29 @@ package arquitectura.software.demo
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-
+import org.springframework.amqp.rabbit.connection.ConnectionFactory
+import org.springframework.boot.ApplicationRunner
 @SpringBootApplication
 @Configuration
 @EnableDiscoveryClient
-class DemoApplication
+class DemoApplication{
+	@Bean
+	fun runner(cf: ConnectionFactory): ApplicationRunner {
+		return ApplicationRunner {
+			var open = false
+			while (!open) {
+				try {
+					cf.createConnection().close()
+					open = true
+				} catch (e: Exception) {
+					Thread.sleep(5000)
+				}
+			}
+		}
+	}
+}
 
 fun main(args: Array<String>) {
 	runApplication<DemoApplication>(*args)
